512e4f84d74dbef33f4c5f6ffab9cedae940c59e
dogukanermis <ermisd+github@pm.me>
2022-01-21 19:16:45 +0100

    refactor search algorithm

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import SearchBar from "./SearchBar";
 import { useState } from "react";
 
 function App() {
-  const [searchTerm, setSearchTerm] = useState("");
-  // const [coinFilter, setCoinFilter] = useState(searchTerm);
+  const [search, setSearch] = useState("");
 
   const handleSearch = (e) => {
-    setSearchTerm(e.target.value.toLowerCase());
+    setSearch(e.target.value);
   };
+
+  const filteredCoins = coinList.filter((coin) =>
+    coin.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div className="App">
@@ -17,16 +20,12 @@ function App() {
       <h2 id="titleLabel">
         a lightweight, open-source, universal paper wallet generator
       </h2>
-      <SearchBar coins={coinList} handleSearch={handleSearch} />
-      {!searchTerm ? (
-        <ol>
-          {coinList.map((coin, index) => (
-            <li key={index}>{coin}</li>
-          ))}
-        </ol>
-      ) : (
-        coinList.filter((coin) => coin.toLowerCase().includes(searchTerm))
-      )}
+      <SearchBar handleSearch={handleSearch} />
+      <ol>
+        {filteredCoins.map((coin, index) => (
+          <li key={index}>{coin}</li>
+        ))}
+      </ol>
     </div>
   );
 }
